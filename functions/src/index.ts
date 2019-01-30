ee04686555bc2e2864cb39ab135bb81961b30d52
jeansampen <jeansampen93@gmail.com>
2019-01-30 14:19:06 +0100

    sending data instead of notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,30 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+
 admin.initializeApp();
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-export const testEvent = functions.database
+export const newEvent = functions.database
   .ref("/events/{eventId}")
   .onCreate((snapshot, context) => {
     let eventId = context.params.eventId;
     let event = snapshot.val();
     console.log("Created event with id ", eventId, " at ", event.place);
     var payload = {
-      notification: {
-        title: "Hi from Realtime database with id = " + eventId,
-        body: "New event at " + event.place + " has been created"
+      data: {
+        title: "DataTitle = " + eventId,
+        body: "DataBody = " + event.place
       }
     };
-    let token =
-      "cJYDx5adUeg:APA91bHqMH8rk9n5W681mB8RRNVZECMm72tKT7hgQ1uaZwuIq4BRFHFN2P3fMyPtUA1CxVv3VMV9ltyA2V5NcCXiD-i7E_h7JQnCb2Cc9rguHZSAzEp5qB0DISWUZH7-FEfpEmPgQ2Xg";
-    admin
-      .messaging()
-      .sendToDevice(token, payload)
-      .then(() => {});
-
-    return "Wassup!";
+    let ref = admin.database().ref("/players/")
+    return ref.once("value", snapshot => {
+        let players = snapshot.val()
+        let tokens = [] as string[];
+        Object.keys(players).forEach(key => {
+            let token = players[key]["fcm_token"]
+            tokens.push(token)
+            console.log(token)
+        })
+        admin
+        .messaging()
+        .sendToDevice(tokens, payload)
+    });
   });
