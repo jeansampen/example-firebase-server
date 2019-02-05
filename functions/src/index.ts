5c017e8b758d5bd380de764aae228f6af4afb33c
jeansampen <jeansampen93@gmail.com>
2019-02-05 23:27:47 +0100

    adding and uploading events is now done from firebase cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -29,15 +29,41 @@ export const onEventAdded = functions.database
 
 export const player = functions.https.onRequest((req, res) => {
   let ref_to_players = admin.database().ref("/players/");
-
   if (req.method === "POST") {
+    console.log("POST");
     let player = req.body;
     return ref_to_players.push(player).then(() => {
       res.send(player);
     });
-  } else if (req.method === "GET") {
+  }
+
+  else if (req.method === "GET") {
+	  console.log("GET");
+	  return ref_to_players.once("value").then(snapshot => {
+		  let players = snapshot.val();
+		  res.send(players);
+    });
   }
   return;
 });
 
-export const event = functions.https.onRequest((req, res) => {});
+export const event = functions.https.onRequest((req, res) => {
+	let ref_to_events = admin.database().ref("/events/");
+  if (req.method === "POST") {
+    console.log("POST");
+    let event = req.body;
+    return ref_to_events.push(event).then(() => {
+      res.send(event);
+    });
+  }
+
+  else if (req.method === "GET") {
+	  console.log("GET");
+	  return ref_to_events.once("value").then(snapshot => {
+		  let events = snapshot.val();
+		  res.send(events);
+    });
+  }
+  return;
+
+});
