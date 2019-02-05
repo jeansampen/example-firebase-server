8cd5eedfdd1d21c5f2139a2a4d986cfc5e48099b
jeansampen <jeansampen93@gmail.com>
2019-02-05 22:59:38 +0100

    migrating all services to firebase cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
-export const newEvent = functions.database
+export const onEventAdded = functions.database
   .ref("/events/{eventId}")
   .onCreate((snapshot, context) => {
     let eventId = context.params.eventId;
@@ -14,17 +14,30 @@ export const newEvent = functions.database
         body: "DataBody = " + event.place
       }
     };
-    let ref = admin.database().ref("/players/")
+    let ref = admin.database().ref("/players/");
     return ref.once("value", snapshot => {
-        let players = snapshot.val()
-        let tokens = [] as string[];
-        Object.keys(players).forEach(key => {
-            let token = players[key]["fcm_token"]
-            tokens.push(token)
-            console.log(token)
-        })
-        admin
-        .messaging()
-        .sendToDevice(tokens, payload)
+      let players = snapshot.val();
+      let tokens = [] as string[];
+      Object.keys(players).forEach(key => {
+        let token = players[key]["fcm_token"];
+        tokens.push(token);
+        console.log(token);
+      });
+      admin.messaging().sendToDevice(tokens, payload);
     });
   });
+
+export const player = functions.https.onRequest((req, res) => {
+  let ref_to_players = admin.database().ref("/players/");
+
+  if (req.method === "POST") {
+    let player = req.body;
+    return ref_to_players.push(player).then(() => {
+      res.send(player);
+    });
+  } else if (req.method === "GET") {
+  }
+  return;
+});
+
+export const event = functions.https.onRequest((req, res) => {});
---
+++ functions/src/services/notification-service.ts
@@ -0,0 +1,5 @@
+export const notify = (tokens: string[], payload: any) => {
+    console.log("Notifying!!!")
+    console.log("Used tokens = ", tokens)
+    console.log("Payload = ", payload)
+}
